e70318fa8721b5e2413821377a38fd38b92ff35a
Nicolas Frey <nicolas.theo.frey@gmail.com>
2026-09-02 22:54:10 +0200

    tiling: refactor LOD API and add tests
    refactor a bunch of stuff for future commit to include a visual LOD
    example. The tests where written by Claude as I can't be bothered to
    formulate a whole unit test suite.
    
    The math for a lot of this stuff is based off of a C#/Unity project.
    
    Signed-off-by: Nicolas Frey <nicolas.theo.frey@gmail.com>

--- tiling/src/osm.rs
+++ tiling/src/osm.rs
@@ -1,1 +1,46 @@
-//! TODO
+use convection_types::BBox;
+
+use crate::{
+    factory::TileFactory,
+    tile::{Tile, TileImage, TilingScheme, WebMercatorScheme},
+};
+
+use reqwest::blocking::{Client as HttpClient, Response};
+
+pub struct OsmTileFactory {
+    scheme: WebMercatorScheme,
+    client: HttpClient,
+}
+
+impl TileFactory for OsmTileFactory {
+    type Scheme = WebMercatorScheme;
+
+    fn new() -> Self {
+        Self::with_scheme(WebMercatorScheme::default())
+    }
+
+    fn with_scheme(scheme: Self::Scheme) -> Self {
+        Self {
+            scheme,
+            client: HttpClient::new(),
+        }
+    }
+
+    fn scheme(&self) -> &Self::Scheme {
+        &self.scheme
+    }
+
+    fn cache_namespace(&self) -> Option<String> {
+        Some("open-street-map".to_owned())
+    }
+
+    fn rendered_tile(&self, tile: Tile) -> anyhow::Result<TileImage> {
+        let bbox = self.scheme.bounds(tile);
+        todo!()
+        // let req = self.build_request(bbox);
+        // let bytes = self.client.process_image(&req)?;
+        // let image = image::load_from_memory(&bytes)?.into_rgba8();
+
+        // Ok(TileImage::new(tile, image))
+    }
+}
